cb82737b2cf46139ab4d252b6ec62c8b77464720
Drangello <drangello0505@gmail.com>
2026-04-22 11:03:59 +0200

    docs: add a newline for better readability in the setup instructions

--- auth_app/admin.py
+++ auth_app/admin.py
@@ -2,14 +2,21 @@ from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 from .models import User
 
+
 class CustomUserAdmin(UserAdmin):
     model = User
     list_display = ['email', 'username', 'fullname', 'is_staff', 'is_active']
     search_fields = ['email', 'username', 'fullname']
-    
-    # Optional fieldsets to show fullname if we use standard UserAdmin forms
+
     fieldsets = UserAdmin.fieldsets + (
         ('Extra Profil-Daten', {'fields': ('fullname',)}),
     )
+
+    def has_delete_permission(self, request, obj=None):
+        if request.user.is_superuser:
+            return True
+
+        return request.user.is_staff
+
 
 admin.site.register(User, CustomUserAdmin)
